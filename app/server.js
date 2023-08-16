0656ccae7ee914aea26ce1ef6789c547e3161c62
Apostolos Polymenakos <128686028+apos-acn@users.noreply.github.com>
2023-08-16 13:38:26 +0100

    Update server.js

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ app.use(morgan('combined'));
 
 // Configuration
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "Hello world!";
+var message = process.env.MESSAGE || "Hello OpenShift!";
 
 app.get('/', function (req, res) {
     res.render('home', createResponseObject());
